a9f1ef11547d4a79da688c166fd33d0ad1a8aaca
AntonLop <antoniotaera@gmail.com>
2025-04-14 05:43:31 -0400

    termine

---
+++ app.js
@@ -0,0 +1,45 @@
+const fullImgBox = document.getElementById("fullImgbox"),
+      imgElement = document.getElementById("imgELement"),
+      rightBtn= document.getElementById("rightBTN"),
+      leftBtn= document.getElementById("leftBTN"),
+      closeImg = document.getElementById("close"),
+      listaImg = [...document.querySelectorAll(".img")];
+
+let indexImf =0
+
+listaImg.forEach((img,i)=>{
+    img.addEventListener("click", (e) =>{
+        clicked(i)
+        fullImgBox.style.display="flex";
+        imgElement.src= img.src
+    })
+})
+
+function clicked(position){
+    indexImf = position;
+    imgElement.src= listaImg[indexImf].src
+}
+rightBtn.addEventListener("click", (e) =>{
+    if (indexImf>=listaImg.length-1) {
+        indexImf=-1
+    }
+    indexImf++;
+    imgElement.src= listaImg[indexImf].src
+})
+leftBtn.addEventListener("click", (e) =>{
+    if (indexImf<= 0) {
+        indexImf=listaImg.length
+    }
+    indexImf--;
+    imgElement.src= listaImg[indexImf].src
+})
+fullImgBox.style.display="flex";
+
+closeImg.addEventListener("click",e=>{fullImgBox.style.display="none";})
+
+fullImgBox.addEventListener("click",e=>{
+   if (e.target.classList.contains("fullImage")) {
+    fullImgBox.style.display="none";
+   
+   }
+})
